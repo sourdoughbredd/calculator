c914e3a99f0fe3882233478d50f3bf03e76e20fa
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-09-20 08:31:38 -0400

    Add operate function and divide by zero protection

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let firstOperand, secondOperand, operator;
+
 const add = function(a, b) {
 	return a + b;
 };
@@ -11,8 +13,24 @@ const multiply = function(a, b) {
 };
 
 const divide = function(a, b) {
-    return a / b;
+    if (b !== 0) {
+        return a / b;
+    }
+    alert('Divide by zero! Microsoft just went down 3 points!')
 };
 
-
-
+const operate = function(operator, a, b) {
+    // Apply operator to a and b. Ex: operate('-', a, b) = a - b;
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            break;
+    }
+}
